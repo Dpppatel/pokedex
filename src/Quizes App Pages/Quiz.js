b78b6a340bdf5ddd103dcfcf927b3d7ea3a433ce
Deep <105926444+Dpppatel@users.noreply.github.com>
2024-01-04 13:30:28 -0600

    dont know what this is

--- src/Quizes App Pages/Quiz.js
+++ src/Quizes App Pages/Quiz.js
@@ -20,7 +20,7 @@ const Quiz = ({pokedex, selection}) => {
 
     const change = (event) =>{
         setInput(event.target.value)
-        if(list.find(el => el.pokemon_species.name === event.target.value.toLowerCase()) && !track.includes(event.target.value)){
+        if(list.find(el => el.name === event.target.value.toLowerCase()) && !track.includes(event.target.value)){
             setList(list.filter(el => el !== event.target.value));
             setScore(score+ 1);
             setTrack([...track, event.target.value]);
@@ -39,7 +39,7 @@ const Quiz = ({pokedex, selection}) => {
     <div className="quiz">
         <h2>There are {list.length} pokemons in {selection} region.  Can you guess'em all?</h2><h4>Your score: {`${score}/${list.length}`}</h4>
         <input  className='searchbar' type="text" placeholder="Search for a Pokemon" onChange={change} value={input}/>
-        <div className="pokemonsImage">{list.map((el,i) => (<img key={i} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${el.pokemon_species.url.split('/').slice(-2, -1).slice(0)}.png`} />))}
+        <div className="pokemonsImage">{list.map((el,i) => (<img key={i} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${el.url.split('/').slice(-2, -1).slice(0)}.png`} />))}
         </div>
     </div>)
 }
